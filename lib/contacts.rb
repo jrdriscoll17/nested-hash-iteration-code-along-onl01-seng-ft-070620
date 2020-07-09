879b4e2f6d6e3facfaa9b35d6ad39084f62d84df
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-09 05:37:23 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,10 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  
+  contacts["Freddy Mercury"].each do |key|
+    if key == favorite_ice_cream_flavors
+      favorite_ice_cream_flavors.each do |flavor| 
+        if flavor == "strawberry"
+          
+  end
 end
